e0c48fa8d560c07037d90158c8008aec8ab040e9
Chico204 <olatunjimark391@gmail.com>
2025-05-30 01:21:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+import List from './list';
+import Alert from './alert';
+
+function App() {
+ return(
+  <div>Grocery bud</div>
+ )
+}
+
+export default App;
---
+++ src/list.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { FaEdit, FaTrash } from 'react-icons/fa';
+function List (){
+
+}
+export default List
