4a882b19da0c7885578d7331867babfcb2fee431
Chico204 <olatunjimark391@gmail.com>
2025-05-30 01:52:48 +0100

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,67 @@ import List from './list';
 import Alert from './alert';
 
 function App() {
+  const [name, setName] = useState('')
+ const [list, setList] = useState('')
+  const [isEditing, setIsEditing] = useState(false)
+   const [editID, setEditID] = useState(null)
+    const [alert, setAlert] = useState({show:false, msg:'', type:'', })
+
+    const handleSubmit = (e) => {
+      e.preventDefault()
+      if(!name){
+        showaAlert(true, 'danger' , 'please enter value')
+      }else if(name && isEditing){
+        setList(
+          list.map((item) => {
+            if (item.id === editID){
+              return {...item, title:name}
+            }
+            return item
+          })
+        )
+        setName('')
+        setEditID(null)
+        setIsEditing(false)
+         showAlert(true, 'success', 'value changed');
+      }else {
+      showAlert(true, 'success', 'item added to the list');
+      const newItem = { id: new Date().getTime().toString(), title: name };
+
+      setList([...list, newItem]);
+      setName('');
+    }
+ }
+
+ const showAlert = (show = false, type ='', msg='',) =>{
+  setAlert({show,type,msg})
+ }
+const clearList = ()=> {
+  showAlert(true, 'danger' , 'item removed')
+  setList(list.filter((item)=> item.id != id))
+}
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    setIsEditing(true);
+    setEditID(id);
+    setName(specificItem.title);
+  }
+
+ useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list));
+  }, [list])
+
  return(
-  <div>Grocery bud</div>
+  <section className='section-center'>
+    <form className='grocery-form' onSubmit={handleSubmit}>
+      <h3>Grocery Bud</h3>
+      <div className='underline'></div>
+      <div className='form-control'>
+    <input type="text" className='grocery' placeholder='e.g eggs' value={name}  />
+      </div>
+    </form>
+
+  </section>
  )
 }
 
